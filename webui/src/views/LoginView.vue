35571f65c0680f1e9059a17f765440b25a0e3294
DavideF5 <marincione.1927757@studenti.uniroma1.it>
2022-12-22 13:39:57 +0100

    Modified default login stuff

--- webui/src/views/LoginView.vue
+++ webui/src/views/LoginView.vue
@@ -30,7 +30,7 @@ export default {
 <template>
 	<div>
         <div class="justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
-            <h5 class="h5">Login:</h5> <input v-on:keyup.enter="login" v-model="input" type="text" placeholder="handle's name" />
+            <h5 class="h5">Login:</h5> <input v-on:keyup.enter="login" v-model="input" type="text" placeholder="username" />
             <p> The response is: <b>{{some_data}}</b> </p>
 		</div>
 
